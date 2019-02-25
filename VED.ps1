70967a8f910525a04f119f20c8561c1e60258184
LoganWestbury <logan.westbury@gmail.com>
2019-02-25 11:13:50 +0000

    Add files via upload

---
+++ VED.ps1
@@ -0,0 +1,48 @@
+﻿
+
+CLS
+
+# GET USER INPUTS
+
+$expiryDay = Read-Host("Enter the expiry day")
+$expiryMonth = Read-Host("Enter the expiry month")
+$expiryYear = Read-Host("Enter the expiry year")
+
+# CALCULATE START DATE FROM EXPIRY DATE
+
+#[datetime]::Now.ToString([cultureinfo]::CreateSpecificCulture('en-GB'))
+
+$cultureENGB = New-Object System.Globalization.CultureInfo("en-GB")
+
+$expiryDate = ([datetime]($expiryMonth + "/" + $expiryDay + "/" + $expiryYear))
+
+$startDate = (($expiryDate.AddYears(-5).AddDays(1)))
+
+$expiryDateFormatted = ($expiryDate).toString("dd/MM/yyyy", $cultureENGB)
+
+$startDateFormatted = ($startDate).toString("dd/MM/yyyy", $cultureENGB)
+
+# DISPLAY DATES TO CONSOLE
+
+Write-Host ("========================================")
+
+Write-Host ("Start Date  = " + $startDateFormatted)
+
+Write-Host ("========================================")
+
+Write-Host ("Expiry Date = " + $expiryDateFormatted)
+
+Write-Host ("========================================")
+
+
+
+
+
+
+
+
+
+
+#Write-Host ($expiryDate.Day + " " + $expiryDate.Date + " " + $expiryDate.Month + " " + $expiryDate.Year)
+
+
