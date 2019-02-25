261e3a64399b12789bea3b9346db36ad9f756d47
LoganWestbury <logan.westbury@gmail.com>
2019-02-25 11:31:51 +0000

    cleaned up and added comments
    cleaned up and added comments

--- VED.ps1
+++ VED.ps1
@@ -1,48 +1,27 @@
-﻿
+﻿# Clear console
+Clear
 
-CLS
-
-# GET USER INPUTS
-
+# Get user inputs
 $expiryDay = Read-Host("Enter the expiry day")
 $expiryMonth = Read-Host("Enter the expiry month")
 $expiryYear = Read-Host("Enter the expiry year")
 
-# CALCULATE START DATE FROM EXPIRY DATE
-
-#[datetime]::Now.ToString([cultureinfo]::CreateSpecificCulture('en-GB'))
-
+# Set date format to UK 
 $cultureENGB = New-Object System.Globalization.CultureInfo("en-GB")
 
+# Parse expiry date into datetime object
 $expiryDate = ([datetime]($expiryMonth + "/" + $expiryDay + "/" + $expiryYear))
 
+# Calculate start date by going back 5 years and adding 1 day 
 $startDate = (($expiryDate.AddYears(-5).AddDays(1)))
 
+# Format both dates into dd/MM/YYYY
 $expiryDateFormatted = ($expiryDate).toString("dd/MM/yyyy", $cultureENGB)
-
 $startDateFormatted = ($startDate).toString("dd/MM/yyyy", $cultureENGB)
 
-# DISPLAY DATES TO CONSOLE
-
+# Display outputs to console
 Write-Host ("========================================")
-
 Write-Host ("Start Date  = " + $startDateFormatted)
-
 Write-Host ("========================================")
-
 Write-Host ("Expiry Date = " + $expiryDateFormatted)
-
 Write-Host ("========================================")
-
-
-
-
-
-
-
-
-
-
-#Write-Host ($expiryDate.Day + " " + $expiryDate.Date + " " + $expiryDate.Month + " " + $expiryDate.Year)
-
-
